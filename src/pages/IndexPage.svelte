a5e1662ae7a0d7b6141f56570219aadeb68b6918
Ömer YAZICIOĞLU <oyazicioglu@hotmail.com>
2022-01-26 21:02:03 +0300

    Add pages for all components

---
+++ src/pages/IndexPage.svelte
@@ -0,0 +1,5 @@
+<script>
+	import Container from '../components/layout/container.svelte';
+</script>
+
+<Container />
---
+++ src/pages/LayoutPage.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Row from '../components/layout/row.svelte';
+	import Container from '../components/layout/container.svelte';
+	import Column from '../components/layout/column.svelte';
+</script>
+
+<Container>
+	<Row>
+		<Column>test</Column>
+	</Row>
+</Container>
